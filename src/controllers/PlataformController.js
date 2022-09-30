4dbab78ea47bc6ed99b04455239c0bc251969bed
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2022-09-30 14:51:41 -0500

    Added new routes and PlataformController methods

--- src/controllers/PlataformController.js
+++ src/controllers/PlataformController.js
@@ -35,18 +35,326 @@ const LearningPaths = [
     description:
       'Hola dev, aquí encontrarás la ruta sugerida por Trinity FS si deseas convertirte en desarrollador Front-End <Disfruta del proceso />',
     link: '/aprender/frontend',
+    image:
+      'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
   },
   {
     title: 'Ruta Backend',
     description:
       'Hola dev, aquí encontrarás la ruta sugerida por Trinity FS si deseas convertirte en desarrollador Back-End <Disfruta del proceso />',
     link: '/aprender/backend',
+    image:
+      'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
   },
   {
     title: 'Ruta Complementos',
     description:
       'Hola dev, Aquí encontrarás temas que te puede ayudar de complemento  <Disfruta del proceso />',
     link: '/aprender/complementos',
+    image:
+      'https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
+  },
+]
+
+const FrontendPath = {
+  title: 'Ruta de Frontend',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic! Dolore reiciendis labore tenetur voluptate consequuntur consectetur quod eos, tempora debitis repellendus, dolores veritatis. At.',
+  topics: [
+    {
+      topic: 'HTML',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/HTML/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/HTML/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/HTML/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'CSS',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/CSS/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/CSS/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/CSS/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'JavaScript',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/JavaScript/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/JavaScript/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/JavaScript/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'TypeScript',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/TypeScript/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/TypeScript/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/TypeScript/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'React',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/React/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/React/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/React/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'Angular',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/Angular/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/Angular/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/Angular/avanzado',
+        },
+      ],
+    },
+  ],
+}
+
+const BackendPath = {
+  title: 'Ruta Backend',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic! Dolore reiciendis labore tenetur voluptate consequuntur consectetur quod eos, tempora debitis repellendus, dolores veritatis. At.',
+  topics: [
+    {
+      topic: 'Topic 1',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic1/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic1/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic1/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'Topic 2',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic2/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic2/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic2/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'Topic 3',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic3/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic3/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic3/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'Topic 4',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic4/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic4/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic4/avanzado',
+        },
+      ],
+    },
+    {
+      topic: 'Topic 5',
+      sections: [
+        {
+          level: 'Principiante',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic5/principiante',
+        },
+        {
+          level: 'Intermedio',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic5/intermedio',
+        },
+        {
+          level: 'Avanzado',
+          description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, minus quo tempore laboriosam amet hic!',
+          link: '/aprender/topic5/avanzado',
+        },
+      ],
+    },
+  ],
+}
+
+const ContentsHTML = [
+  {
+    topic: 'HTML',
+    level: 'Principiante',
+    contents: [
+      {
+        order: 1,
+        title: '',
+        description: '',
+        link: '',
+      },
+      {
+        order: 2,
+        title: '',
+        description: '',
+        link: '',
+      },
+      {
+        order: 3,
+        title: '',
+        description: '',
+        link: '',
+      },
+      {
+        order: 4,
+        title: '',
+        description: '',
+        link: '',
+      },
+    ],
   },
 ]
 
@@ -62,6 +370,44 @@ class PlataformController {
   getListOfRecommendations = async (req, res, next) => {
     try {
       return res.status(202).json(Recommendations).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  getAllTopicsOfFrontend = async (req, res, next) => {
+    try {
+      return res.status(202).json(FrontendPath).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  getAllTopicsOfBackend = async (req, res, next) => {
+    try {
+      return res.status(202).json(BackendPath).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  getAllTopicsOfComplementos = async (req, res, next) => {
+    try {
+      return res.status(202).json(BackendPath).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  getContentsByTopicAndLevel = async (req, res, next) => {
+    try {
+      const { topic, level } = req.params
+      if (
+        topic.toLowerCase() === 'html' &&
+        level.toLowerCase() === 'principiante'
+      ) {
+        return res.status(202).json(ContentsHTML).end()
+      }
     } catch (e) {
       next(e)
     }
--- src/routes/plataform.routes.js
+++ src/routes/plataform.routes.js
@@ -4,6 +4,13 @@ const router = Router()
 
 router.get('/paths', plataformController.getLearningPaths)
 router.get('/recommendations', plataformController.getListOfRecommendations)
+router.get('/topics/frontend', plataformController.getAllTopicsOfFrontend)
+router.get('/topics/backend', plataformController.getAllTopicsOfBackend)
+router.get(
+  '/topics/complementos',
+  plataformController.getAllTopicsOfComplementos
+)
+router.get('/:topic/:level', plataformController.getContentsByTopicAndLevel)
 
 const plataformRouter = router
 module.exports = { plataformRouter }
